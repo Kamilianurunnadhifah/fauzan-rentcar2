ab2818ef55b2bb9f5abdd8029829dbf098dd1347
kam <92259786+Kamilianurunnadhifah@users.noreply.github.com>
2025-11-17 16:12:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ if (bookingForm) {
     e.preventDefault();
     const fd = new FormData(bookingForm);
     const data = Object.fromEntries(fd.entries());
-    const message = `*Booking Fauzan Rentcar*%0A%0A*Nama:* ${data.name}%0A*Mulai:* ${data.startDate}%0A*Tujuan:* ${data.destination}%0A*Lama:* ${data.duration}%0A*Mobil:* ${data.carType}%0A%0ATerima kasih!`;
+    const message = `*Halo, Fauzan Rentcar, saya ingin mengecek ketersediaan mobil:*%0A%0A*Nama:* ${data.name}%0A*Mulai:* ${data.startDate}%0A*Tujuan:* ${data.destination}%0A*Lama Sewa:* ${data.duration}%0A*Jumlah:* ${data.option}%0A*Mobil:* ${data.carType}%0A%0ATerima kasih!`;
     const wa = `https://wa.me/6287754907731?text=${message}`;
     window.open(wa, '_blank');
   });
